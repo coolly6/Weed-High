7e6bff4ec2f32c6aca6d897e2f6c3cdb630fb26e
jennifer yaffe <coolly6@walla.co.il>
2020-08-26 15:37:35 +0300

    setting up the header

---
+++ weed-high/src/components/Header/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../../logo.svg';
+import './logo.css'
+
+function Logo() {
+  return (
+    <div className="logo">
+        <img src={logo} className="logo-img" alt="logo" />
+       <h1 className="logo-title">Weed#High</h1>
+    </div>
+  );
+}
+
+export default Logo;
