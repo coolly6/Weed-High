40f05b325c72b8d2ca614f6080c3d66c9f9c944f
jennifer yaffe <coolly6@walla.co.il>
2020-11-19 09:57:49 +0200

    Revert "styling forms"
    This reverts commit fbad20f1c975e99d8c87b410eeec5a0ed6beb2a3.

--- weed-high/src/components/Header/Header.js
+++ weed-high/src/components/Header/Header.js
@@ -5,19 +5,6 @@ import MenuIcon from './MenuIcon/MenuIcon';
 import SmokeEffect from './SmokeEffect/SmokeEffect';
 
 function Header() {
-<<<<<<< Updated upstream
-=======
-  const { isLightTheme, dark, light } = useContext(ThemeContext);
-  const themeMode = isLightTheme ? light : dark;
-
-  const headerStyles = {
-    background: themeMode.headerBackground,
-    color: themeMode.logoColor,
-    textShadow: themeMode.logoTextShadow,
-    boxShadow:themeMode.headerBoxShadow
-   
-  };
->>>>>>> Stashed changes
   return (
     <div className="header">
         <SmokeEffect/>
